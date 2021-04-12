90361e11e7094703ad3316d32b952ca6d258e579
chinoll <chinoll@protonmail.com>
2021-04-13 00:53:20 +0800

    高性能版

--- runningSum.py
+++ runningSum.py
@@ -1,6 +1,15 @@
+#简单版本
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         result = [0]
         for i in nums:
             result.append(result[-1]+i)
         return result[1:]
+
+#击败95%
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = [0]
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
+        return nums
